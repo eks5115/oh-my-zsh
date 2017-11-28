e8c58956f4f64a7869c9d9e699ecfd65b8831d1b
eks5115 <eks5115@139.com>
2017-11-28 13:15:20 +0800

    # tag to blank space

--- install.sh
+++ install.sh
@@ -9,13 +9,13 @@ cd /tmp/oh-my-zsh/
 
 isLinux=true
 if [ `uname -s` = 'Linux' ]; then
-	 isLinux=true
+  isLinux=true
 else
-	isLinux=false
+  isLinux=false
 fi
 
 if [ ! -n "$ZSH" ]; then
-	ZSH=~/.oh-my-zsh
+  ZSH=~/.oh-my-zsh
 fi
 
 PLUGINS=${ZSH}/plugins
@@ -25,106 +25,106 @@ PLUGINS=${ZSH}/plugins
 # function
 
 inArray() {
-	for i in $2
-	do
-		if [ $1 = ${i} ];then
-			echo true
-			return
-		fi
-	done
+  for i in $2
+  do
+    if [ $1 = ${i} ];then
+      echo true
+      return
+    fi
+  done
 
-	echo false
+  echo false
 }
 
 myPlugins=(`ls ./plugins`)
 
 getPluginNames() {
-	tmpPluginNames=()
-	i=0
-	while(( $i<${#myPlugins[*]} ))
-	do
+  tmpPluginNames=()
+  i=0
+  while(( $i<${#myPlugins[*]} ))
+  do
     tmpPluginNames[${i}]=$(basename ${myPlugins[${i}]} .git.plugin)
     let "i++"
-	done
-	echo ${tmpPluginNames[*]}
+  done
+  echo ${tmpPluginNames[*]}
 }
 pluginNames=(`getPluginNames`)
 ohMyZshPluginNames=('autojump')
 
 pullGitPlugins() {
-	i=0
-	while(( $i<${#myPlugins[*]} ))
-	do
-		if [ ! -e ${PLUGINS}'/'$(basename ${myPlugins[${i}]} .git.plugin) ];then
-			git clone `cat "plugins/${myPlugins[${i}]}"` ${PLUGINS}/$(basename ${myPlugins[${i}]} .git.plugin)
-		fi
+  i=0
+  while(( $i<${#myPlugins[*]} ))
+  do
+    if [ ! -e ${PLUGINS}'/'$(basename ${myPlugins[${i}]} .git.plugin) ];then
+      git clone `cat "plugins/${myPlugins[${i}]}"` ${PLUGINS}/$(basename ${myPlugins[${i}]} .git.plugin)
+    fi
 
-		let "i++"
-	done
+    let "i++"
+  done
 }
 
 
 installZSH() {
-	if [ ! `which zsh` ];then
-		if [ ${isLinux} = true ];then
-			sudo apt-get install zsh
-		else
-			brew install zsh
-		fi
-	fi
+  if [ ! `which zsh` ];then
+    if [ ${isLinux} = true ];then
+      sudo apt-get install zsh
+    else
+      brew install zsh
+    fi
+  fi
 }
 
 installGit() {
-	if [ ! `which git` ];then
-		if [ ${isLinux} = true ];then
-			sudo apt-get install git
-		else
-			brew install git
-		fi
-	fi
+  if [ ! `which git` ];then
+    if [ ${isLinux} = true ];then
+      sudo apt-get install git
+    else
+      brew install git
+    fi
+  fi
 }
 
 installOhMyZsh() {
-	if [ ! -e ${ZSH} ]; then
-	  sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh|sed '/env zsh$/d')"
-	fi
+  if [ ! -e ${ZSH} ]; then
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh|sed '/env zsh$/d')"
+  fi
 }
 
 installPlugins() {
-#	# plugin zsh-autosuggestions
-#	if [ ! -e ${PLUGINS}/zsh-autosuggestions ]; then
-#		git clone git://github.com/zsh-users/zsh-autosuggestions ${PLUGINS}/zsh-autosuggestions
-#	fi
+#  # plugin zsh-autosuggestions
+#  if [ ! -e ${PLUGINS}/zsh-autosuggestions ]; then
+#    git clone git://github.com/zsh-users/zsh-autosuggestions ${PLUGINS}/zsh-autosuggestions
+#  fi
 #
-#	# plugin zsh-autosuggestions
-#	if [ ! -e ${PLUGINS}/zsh-syntax-highlighting ]; then
-#		git clone git://github.com/zsh-users/zsh-syntax-highlighting.git ${PLUGINS}/zsh-syntax-highlighting
-#	fi
+#  # plugin zsh-autosuggestions
+#  if [ ! -e ${PLUGINS}/zsh-syntax-highlighting ]; then
+#    git clone git://github.com/zsh-users/zsh-syntax-highlighting.git ${PLUGINS}/zsh-syntax-highlighting
+#  fi
 
-	pullGitPlugins
+  pullGitPlugins
 
-	newPluginsArray=(${ohMyZshPluginNames[*]} ${pluginNames[*]})
+  newPluginsArray=(${ohMyZshPluginNames[*]} ${pluginNames[*]})
 
-	# write plugin to .zshrc
-	oldPluginsArray=(`sed -n '/^plugins=(/{n; p;}' ~/.zshrc`)
+  # write plugin to .zshrc
+  oldPluginsArray=(`sed -n '/^plugins=(/{n; p;}' ~/.zshrc`)
 
-	index=0
-	merge=(${oldPluginsArray[*]})
-	length=${#merge[*]}
-	for i in ${newPluginsArray[*]}
-	do
-		if [ ! `inArray ${i} "${oldPluginsArray[*]}"` = true ];then
-			let "index++"
-			let "ind=length+index"
-			merge[${ind}]=${i}
-		fi
-	done
+  index=0
+  merge=(${oldPluginsArray[*]})
+  length=${#merge[*]}
+  for i in ${newPluginsArray[*]}
+  do
+    if [ ! `inArray ${i} "${oldPluginsArray[*]}"` = true ];then
+      let "index++"
+      let "ind=length+index"
+      merge[${ind}]=${i}
+    fi
+  done
 
-	if [ ${isLinux} = true ];then
-		sed -i "/^plugins=(/{n;s/.*/${merge[*]}/;}" ~/.zshrc
-	else
-		sed -i '' "/^plugins=(/{n;s/.*/${merge[*]}/;}" ~/.zshrc
-	fi
+  if [ ${isLinux} = true ];then
+    sed -i "/^plugins=(/{n;s/.*/${merge[*]}/;}" ~/.zshrc
+  else
+    sed -i '' "/^plugins=(/{n;s/.*/${merge[*]}/;}" ~/.zshrc
+  fi
 
 }
 
@@ -150,8 +150,9 @@ installPlugins
 
 ###
 # source
-if [ "/bin/zsh" != "${SHELL}" ];then
-	env zsh
-fi
 
 cd ~
+
+if [ "/bin/zsh" != "${SHELL}" ];then
+  env zsh
+fi
